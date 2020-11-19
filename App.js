e5c746946cd8455004f75ae28925521f08742935
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-11-19 19:38:26 -0300

    navagation

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import { View, Text, StyleSheet } from 'react-native';
 import Home from './src/View/home/home'
 import Accounts from './src/View/accounts/accounts'
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+
 export default function App() {
  
     return (
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Desenvolvedores" component={Accounts} />
+        </Stack.Navigator>
+      </NavigationContainer>
 
     );
   
